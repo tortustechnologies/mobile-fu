d0316ed312859518b5c695d9de3fc832fb7515b2
William Makley <wmakley@tortus.com>
2011-11-10 16:06:43 -0500

    don't detect ipads as mobile devices

--- lib/mobile_fu.rb
+++ lib/mobile_fu.rb
@@ -6,7 +6,7 @@ module ActionController
                           'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
                           'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
                           'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
-                          'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+                          'webos|amoi|novarra|cdm|alcatel|pocket|iphone|mobileexplorer|' +
                           'mobile'
     
     def self.included(base)
